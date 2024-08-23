577b2d9ed598a6b8875e7d666e37fadb5c4b9ada
BloxyFolabi <112190562+Fjcoolkid@users.noreply.github.com>
2024-08-23 18:44:24 +0100

    Add files via upload

---
+++ BloxyDosV1.py
@@ -0,0 +1,31 @@
+#fake script just ment to look like an actual script cus i cant make an actual script i got limited knoledge innit so ima try learn more. made by bloxyfolabi
+import time
+print("running BLOXYDOSV1...\n")
+time.sleep(1)
+print("loading bots...\n")
+time.sleep(3)
+print("loading scripts...\n ")
+time.sleep(4)
+print("loading bot scripts\n")
+time.sleep(1)
+permission = input("do you want to continue with this ddos script?\n")
+if permission == "yes":
+         print("THIS SCRIPT WAS MADE BY FoLaBi :)))")
+         time.sleep(1)
+         print("\033[1;32;40m B L O X Y D O S V1 \n")
+         option = input("\033[1;32;40m [1] Website DDoS\n [2] IP DDos DDoS\n \033[1;35;40m[3] Contact me\033[1;32;40m\n ")
+         if option == "1":
+             target1 = input("\033[1;35;40m Website:  ")
+             port = input("\033[1;35;40m Port (80 or 144 recommended):  ")
+             print("Loading Bot servers and DDoS")
+             time.sleep(2)
+             print("Packet sent lol /n" * 500)
+             
+             
+if option == "2":
+ target2 = input("\033[1;35;40m IP:  ")
+ port2 = input("\033[1;35;40m Port: ")
+ print("Packet sent lol \n"  * 500)        
+ 
+ if option == "3":
+     print("\033[1;31g;40m YT: BloxyFolabi\n TT: BloxyFolabi\n Github: BloxyFolabi")
